4700085f148e1b4ac03de2399268ed885f08fda0
Matt Stockdale <mstockdale@logicworks.net>
2015-05-08 16:35:04 -0400

    Updated to utilize v3 YouTube API, and added API key field to Prefs

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,4 @@
-YOUTUBE_VIDEO_DETAILS = 'http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc'
+YOUTUBE_VIDEO_DETAILS = 'https://www.googleapis.com/youtube/v3/videos/?id=%s&part=snippet&key=%s'
 RE_YT_ID = Regex('[a-z0-9\-_]{11}', Regex.IGNORECASE)
 
 def Start():
@@ -33,24 +33,22 @@ class YouTubeAgent(Agent.Movies):
 
 	def update(self, metadata, media, lang):
 		try:
-			json_obj = JSON.ObjectFromURL(YOUTUBE_VIDEO_DETAILS % metadata.id)['data']
+			json_obj = JSON.ObjectFromURL(YOUTUBE_VIDEO_DETAILS % (metadata.id, Prefs['yt_apikey']))['items'][0]['snippet']
 		except:
 			Log('Could not retrieve data from YouTube API for: %s' % metadata.id)
 			json_obj = None
 
 		if json_obj:
 			metadata.title = json_obj['title']
-			metadata.studio = json_obj['uploader']
-			metadata.rating = json_obj['rating'] * 2
+			metadata.studio = json_obj['channelTitle']
 			metadata.summary = json_obj['description']
-			metadata.duration = json_obj['duration'] * 1000
-			metadata.originally_available_at = Datetime.ParseDate(json_obj['uploaded']).date()
+			metadata.originally_available_at = Datetime.ParseDate(json_obj['publishedAt']).date()
 
 			thumb = None
-			if 'hqDefault' in json_obj['thumbnail']:
-				thumb = json_obj['thumbnail']['hqDefault']
-			elif 'sqDefault' in json_obj['thumbnail']:
-				thumb = json_obj['thumbnail']['sqDefault']
+			if 'high' in json_obj['thumbnails']:
+				thumb = json_obj['thumbnails']['high']['url']
+			elif 'standard' in json_obj['thumbnails']:
+				thumb = json_obj[0]['thumbnails']['standard']['url']
 
 			if thumb:
 				poster = HTTP.Request(thumb)
